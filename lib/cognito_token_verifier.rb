08a3d8658a9e6e476f7f608f630147846fa84c94
Matt Tanous <mtanous22@gmail.com>
2019-02-19 14:46:42 -0500

    Initial commit

---
+++ lib/cognito_token_verifier.rb
@@ -0,0 +1,6 @@
+require "cognito_token_verifier/version"
+
+module CognitoTokenVerifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cognito_token_verifier/version.rb
@@ -0,0 +1,3 @@
+module CognitoTokenVerifier
+  VERSION = "0.1.0"
+end
