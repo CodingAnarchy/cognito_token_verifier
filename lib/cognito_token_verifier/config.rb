ee9da86e6c2df29de253ad58860c7cc1a75f1795
Ryan Romanchuk <rromanchuk@gmail.com>
2022-05-18 16:31:46 -0700

    example thought experiments

--- lib/cognito_token_verifier/config.rb
+++ lib/cognito_token_verifier/config.rb
@@ -15,18 +15,22 @@ module CognitoTokenVerifier
       ['all', 'any', ['id', 'access']].any?{|usage| usage == token_use }
     end
 
+    def validate!
+      raise ConfigSetupError.new(self) unless aws_region.present? and user_pool_id.present?
+    end
+
     def allow_expired_tokens?
       allow_expired_tokens
     end
 
-    def jwks
-      begin
-        raise ConfigSetupError.new(self) unless aws_region.present? and user_pool_id.present?
-        @jwks ||= JSON.parse(RestClient.get(jwk_url))
-      rescue RestClient::Exception, JSON::JSONError => e
-        raise JWKFetchError
-      end
-    end
+    # def jwks
+    #   begin
+    #     raise ConfigSetupError.new(self) unless aws_region.present? and user_pool_id.present?
+    #     @jwks ||= JSON.parse(RestClient.get(jwk_url))
+    #   rescue RestClient::Exception, JSON::JSONError => e
+    #     raise JWKFetchError
+    #   end
+    # end
 
     # TODO Because the load balancer does not encrypt the user claims, we
     # recommend that you configure the target group to use HTTPS. If you
@@ -38,14 +42,14 @@ module CognitoTokenVerifier
     # is as follows:
     #  https://public-keys.auth.elb.region.amazonaws.com/key-id
     # See: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html
-    def iss
-      "https://cognito-idp.#{aws_region}.amazonaws.com/#{user_pool_id}"
-    end
+    # def iss
+    #   "https://cognito-idp.#{aws_region}.amazonaws.com/#{user_pool_id}"
+    # end
 
     private
 
-    def jwk_url
-      "#{iss}/.well-known/jwks.json"
-    end
+    # def jwk_url
+    #   "#{iss}/.well-known/jwks.json"
+    # end
   end
 end
--- lib/cognito_token_verifier/token.rb
+++ lib/cognito_token_verifier/token.rb
@@ -2,12 +2,14 @@ require 'json/jwt'
 
 module CognitoTokenVerifier
   class Token
-    attr_reader :header, :decoded_token
+    attr_reader :header, :decoded_token, :idp_type
 
     def initialize(jwt)
       begin
         @header = JSON.parse(Base64.decode64(jwt.split('.')[0]))
-        @jwk = JSON::JWK.new(CognitoTokenVerifier.config.jwks["keys"].detect{|jwk| jwk['kid'] == header['kid']})
+        @idp_type = header["signer"].include?("arn:aws:elasticloadbalancing") ? :alb : :cognito
+
+        @jwk = JSON::JWK.new(jwks["keys"].detect{|jwk| jwk['kid'] == header['kid']})
         @decoded_token = JSON::JWT.decode(jwt, @jwk)
       rescue JSON::JWS::VerificationFailed, JSON::JSONError
         raise TokenDecodingError
@@ -18,12 +20,45 @@ module CognitoTokenVerifier
       decoded_token['exp'] < Time.now.to_i and not CognitoTokenVerifier.config.allow_expired_tokens?
     end
 
+
+    def jwks
+      begin
+        CognitoTokenVerifier.config.validate!
+        @jwks ||= JSON.parse(RestClient.get(jwk_url))
+      rescue RestClient::Exception, JSON::JSONError => e
+        raise JWKFetchError
+      end
+    end
+
+    def jwk_url
+      case idp_type
+      when :alb
+        "#{iss}/#{header['kid']}"
+      else
+        "#{iss}/.well-known/jwks.json"
+      end
+    end
+
+    def iss
+      case idp_type
+      when :alb
+        "https://public-keys.auth.elb.#{CognitoTokenVerifier.config.aws_region}.amazonaws.com"
+      else
+        "https://cognito-idp.#{CognitoTokenVerifier.config.aws_region}.amazonaws.com/#{CognitoTokenVerifier.config.user_pool_id}"
+      end
+    end
+
     def valid_token_use?
       CognitoTokenVerifier.config.any_token_use? || [CognitoTokenVerifier.config.token_use].flatten.include?(decoded_token['token_use'])
     end
 
-    def valid_iss?
-      decoded_token['iss'] == CognitoTokenVerifier.config.iss
+    def alb_claim?
+      idp_type == :alb
     end
+
+    def valid_iss?
+      decoded_token['iss'] == iss
+    end
+
   end
 end
